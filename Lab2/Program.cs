5c45f6bf5c892c0d2fe57eb26ec21b134fe679f2
Mashkins-m-ld <m2402809@edu.misis.ru>
2024-10-12 17:24:07 +0300

    Add files via upload

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Diagnostics.Metrics;
+using System.Globalization;
 using System.Runtime.InteropServices;
 
 public class Program
@@ -21,19 +24,19 @@ public class Program
         //program.Task_1_9(0.9);
         //program.Task_1_10(0.9);
         //program.Task_2_1(10);
-        //program.Task_2_2(5, 3, 2, 1);
+        program.Task_2_2(5, 3, 2, 1);
         //program.Task_2_2(5, 1.5, 1.5, 1);
         //program.Task_2_2(5, 1, 3, 1);
-        //program.Task_2_3(10);
+        //program.Task_2_3(8);
         //program.Task_2_4(5, 1, 2);
-        //program.Task_2_5(10, 30);
+        //program.Task_2_5(10,25.2);
         //program.Task_2_6(5);
-        //program.Task_2_7(5);
-        //program.Task_2_8(5);
-        //program.Task_2_9(10);
-        //program.Task_2_10(10);
-        //program.Task_2_11(10);
-        //program.Task_2_12(10, 0);
+        //program.Task_2_7(4);
+        //program.Task_2_8(4);
+       //program.Task_2_9(10);
+        //program.Task_2_10(6);
+        //program.Task_2_11(8);
+        // program.Task_2_12(10, 0);
         //program.Task_2_13(10, 5, 0);
         //program.Task_3_1();
         //program.Task_3_2(3, 2, 1);
@@ -57,7 +60,9 @@ public class Program
         bool answer = false;
 
         // code here
-
+        double r = 2;
+        if (Math.Abs(x*x+y*y-r*r)<=(1/(10*10*10))) 
+            answer = true;
         // end
 
         return answer;
@@ -67,7 +72,8 @@ public class Program
         bool answer = false;
 
         // code here
-
+        if ((y>=0) && (y+Math.Abs(x)<=1))
+                answer = true;
         // end
 
         return answer;
@@ -77,7 +83,12 @@ public class Program
         double answer = 0;
 
         // code here
-
+        if (a > 0)
+            if (a > b) answer = a;
+            else answer = b;
+        else
+            if (a <b) answer = a;
+            else answer = b;
         // end
 
         return answer;
@@ -85,9 +96,12 @@ public class Program
     public double Task_1_4(double a, double b, double c)
     {
         double answer = 0;
-
         // code here
-
+        double mn = 0;
+        if (a < b) mn = a;
+        else mn=b;
+        if (mn>c) answer= mn;
+        else answer = c;
         // end
 
         return answer;
@@ -95,9 +109,10 @@ public class Program
     public bool Task_1_5(double r, double s)
     {
         bool answer = false;
-
         // code here
-
+        double diameter = Math.Sqrt(r / Math.PI)*2;
+        double diagonal = Math.Sqrt(2) * Math.Sqrt(s);
+        if (diagonal <= diameter) answer = true;
         // end
 
         return answer;
@@ -107,9 +122,10 @@ public class Program
         bool answer = false;
 
         // code here
-
+        double diameter = Math.Sqrt(r / Math.PI) * 2;
+        double side= Math.Sqrt(s);
+        if (side >= diameter) answer = true;
         // end
-
         return answer;
     }
     public double Task_1_7(double x)
@@ -117,6 +133,8 @@ public class Program
         double answer = 0;
 
         // code here
+        if (Math.Abs(x) > 1) answer = 1;
+        else answer = Math.Abs(x);
 
         // end
 
@@ -127,7 +145,8 @@ public class Program
         double answer = 0;
 
         // code here
-
+        if (Math.Abs(x) >= 1) answer = 0;
+        else answer = x * x - 1;
         // end
 
         return answer;
@@ -137,7 +156,9 @@ public class Program
         double answer = 0;
 
         // code here
-
+        if (x <= -1) answer = 0;
+        else if ((x>-1)&&(x<=0)) answer=1+x;
+        else answer = 1;
         // end
 
         return answer;
@@ -147,7 +168,9 @@ public class Program
         double answer = 0;
 
         // code here
-
+        if (x <= -1) answer = 1;
+        else if ((x > -1) && (x <= 1)) answer = -x;
+        else answer = -1;
         // end
 
         return answer;
@@ -158,9 +181,18 @@ public class Program
     public double Task_2_1(int n)
     {
         double answer = 0;
-
         // code here
-
+        int counter = 0;
+        double summa = 0;
+        while (counter < n)
+        {
+            counter++;
+            string input = Console.ReadLine();
+            Double.TryParse(input, out double student);
+            summa += student;
+        }
+        answer = summa / n;
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -170,9 +202,19 @@ public class Program
         int answer = 0;
 
         // code here
-
+        int counter = 0;
+        while (counter < n)
+        {
+            counter++;
+            string inputx= Console.ReadLine();
+            Double.TryParse(inputx,new CultureInfo("en-US"), out double x);
+            string inputy = Console.ReadLine();
+            Double.TryParse(inputy, new CultureInfo("en-US"), out double y);
+            if (((x - a) * (x - a) + (y - b) * (y - b)) <= r * r)
+                answer++;
+        }
         // end
-
+        Console.WriteLine(answer);
         return answer;
     }
     public double Task_2_3(int n)
@@ -180,50 +222,100 @@ public class Program
         double answer = 0;
 
         // code here
-
+        int counter = 0;
         // end
-
+        while (counter < n)
+        {
+            counter++;
+            string input=Console.ReadLine();
+            Double.TryParse(input,new CultureInfo ("en-US"), out double weight);
+            if (weight<30)
+                    answer += 0.2;
+        }
+        Console.WriteLine(answer);
         return answer;
     }
     public int Task_2_4(int n, double r1, double r2)
     {
-        int answer = 0;
-
+        int answer = 0, counter = 0;
         // code here
-
+        while (counter < n)
+        {
+            counter++;
+            Double.TryParse(Console.ReadLine(), new CultureInfo("en-US"),out double x);
+            Double.TryParse(Console.ReadLine(), new CultureInfo("en-US"), out double y);
+            double test = x * x + y * y;
+            if ((r1*r1 <= test) && (test <= r2*r2)) answer++;
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
     }
     public int Task_2_5(int n, double norm)
     {
-        int answer = 0;
+        int answer = 0, counter = 0;
 
         // code here
-
+        while (counter < n)
+        {
+            counter++;
+            string input= Console.ReadLine();
+            Double.TryParse(input, new CultureInfo("en-US"), out double result);
+            if (result<=norm)
+                answer++;
+        }
         // end
-
+        Console.WriteLine(answer);
         return answer;
     }
     public int Task_2_6(int n)
     {
-        int answer = 0;
-
+        int answer = 0, counter = 0;
         // code here
-
+        while (counter < n)
+        {
+            counter++;
+            string inputx = Console.ReadLine();
+            Double.TryParse(inputx, new CultureInfo("en-US"), out double x);
+            string inputy= Console.ReadLine();
+            Double.TryParse(inputy, new CultureInfo("en-US"), out double y);
+            if (y >= 0 && 0 <= x && x <= Math.PI && y <= Math.Sin(x)) 
+                answer++;
+        }
         // end
-
+        Console.WriteLine(answer);
         return answer;
     }
     public (int, int) Task_2_7(int n)
     {
         int answer1 = 0;
         int answer3 = 0;
-
         // code here
-
+        int counter = 0;
+        while (counter < n)
+        {
+            counter++;
+            string inputx = Console.ReadLine();
+            Double.TryParse(inputx, new CultureInfo("en-US"), out double x);
+            string inputy= Console.ReadLine();
+            Double.TryParse (inputy, new CultureInfo("en-US"), out double y);
+            if ((x>0) && (y>0))
+            {
+                Console.WriteLine(1);
+                answer1++;
+            }
+            else if (x < 0 && y > 0) Console.WriteLine(2);
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine(3);
+                answer3++;
+            }
+            else if (x>0 && y<0) Console.WriteLine(4);
+        }
         // end
-
+        Console.WriteLine(answer1);
+        Console.WriteLine(answer3);
         return (answer1, answer3);
     }
     public (int, double) Task_2_8(int n)
@@ -232,9 +324,25 @@ public class Program
         double answerLength = double.MaxValue;
 
         // code here
-
+        int counter = 0;
+        while (counter < n)
+        {
+            counter++;
+            string inputx = Console.ReadLine();
+            Double.TryParse(inputx, new CultureInfo("en-US"), out double x);
+            string inputy = Console.ReadLine();
+            Double.TryParse(inputy, new CultureInfo("en-US"), out double y);
+            double distance = Math.Sqrt(x * x + y * y);
+            if (distance < answerLength)
+            {
+                answer = counter;
+                answerLength = distance;
+            }
+        }
         // end
-
+        answerLength = Math.Round(answerLength, 2);
+        Console.WriteLine(answer);
+        Console.WriteLine(answerLength);
         return (answer, answerLength);
     }
     public double Task_2_9(int n)
@@ -242,9 +350,16 @@ public class Program
         double answer = double.MaxValue;
 
         // code here
-
+        int counter = 0;
+        while (counter < n)
+        {
+            counter++;
+            string input = Console.ReadLine();
+            Double.TryParse(input,new CultureInfo("en-US"), out double result);
+            if (result < answer) answer = result;
+        }
         // end
-
+        Console.WriteLine(answer);
         return answer;
     }
     public int Task_2_10(int n)
@@ -252,9 +367,23 @@ public class Program
         int answer = 0;
 
         // code here;
-
+        int counter = 0;
+        while (counter < n)
+        {
+            counter++;
+            int start = 0, test=0;
+            while (start < 4)
+            {
+                start++;
+                Console.WriteLine($"Студент{counter}.Оценка{start}");
+                string input = Console.ReadLine();
+                Double.TryParse(input, out double result);
+                if (result != 2 && result != 3) test++;
+            }
+            if (test == 4) answer++;
+        }
         // end
-
+        Console.WriteLine(answer);
         return answer;
     }
     public (int, double) Task_2_11(int n)
@@ -263,7 +392,26 @@ public class Program
         double avg = 0.0;
 
         // code here;
-
+        int student = 0;
+        double summa = 0;
+        while (student < n)
+        {
+            student++;
+            int start = 0, test = 0;
+            while (start < 4)
+            {
+                start++;
+                Console.WriteLine($"Введите {start} оценку {student} студента");
+                string input = Console.ReadLine();
+                Double.TryParse(input,out double mark);
+                summa += mark;
+                if (mark > 2) test++;
+            }
+            if (test < 4) answer++;
+        }
+        avg = summa / n / 4;
+        Console.WriteLine(answer);
+        Console.WriteLine(avg);
         // end
 
         return (answer, avg);
@@ -273,7 +421,24 @@ public class Program
         double answer = 0;
 
         // code here;
-
+        if (r < 0)
+            return 0;
+        switch (type)
+        {
+            case 0:
+                    answer = r * r;
+                    break;
+            case 1:
+                    answer = Math.PI * r * r;
+                    break;
+            case 2:
+                answer = r * r * Math.Sqrt(3) / 4;
+                break;
+            default:
+                answer = 0;
+                break;
+        }
+        answer=Math.Round(answer,2);
         // end
 
         return answer;
@@ -283,21 +448,47 @@ public class Program
         double answer = 0;
 
         // code here;
-
+        if ((A<=0) ||(B<=0)) return 0;
+        switch (type)
+        {
+            case 0:
+                answer = A * B;
+                break;
+            case 1:
+                answer = Math.Abs(Math.PI * (A * A - B * B));
+                break;
+            case 2:
+                double p = (A + B + B) / 2;
+                answer = Math.Sqrt(p * (p - A) * (p - B) * (p - B));
+                break;
+            default:
+                answer = 0;
+                break;
+        }
         // end
-
+        answer= Math.Round(answer,2);
         return answer;
     }
     #endregion
 
+    
     #region Level 3
     public double Task_3_1()
     {
         double answer = 0;
         int n = 0;
-
+        double summa = 0;
         // code here
-
+        while (true)
+        {
+            Console.WriteLine("Введите рост ученика, для окончания ввода введите 0");
+            Double.TryParse(Console.ReadLine(), out double height);
+            if (height == 0) break;
+            summa += height;
+            n++;
+        }
+        answer = summa / n;
+        Console.WriteLine(answer);
         // end
 
         // answer should be equal to the task_2_1 answer
@@ -329,7 +520,17 @@ public class Program
         int answer = 0, n = 0;
 
         // code here
-
+        while (true)
+        {
+            Console.WriteLine("Введите координатy x точки, для завершения ввода введите @");
+            string s = Console.ReadLine();
+            if (s == "@") break;
+            Double.TryParse(s, new CultureInfo("en-US"),out double x);
+            Console.WriteLine("Введите координату y точки");
+            Double.TryParse(Console.ReadLine(), new CultureInfo("en-US"),out double y);
+            if ((x * x + y * y >= r1 * r1) && (x * x + y * y <= r2*r2)) answer++;
+        }
+        Console.WriteLine(answer);
         // end
 
         return answer;
@@ -359,9 +560,31 @@ public class Program
         int answer1 = 0, answer3 = 0, n = 0;
 
         // code here
-
+        while (true)
+        {
+            Console.WriteLine("Введите координату x  точки, для прекращения ввода введите stop");
+            string s= Console.ReadLine();
+            if (s == "stop") break;
+            Double.TryParse(s, new CultureInfo("en-US"), out double x);
+            Console.WriteLine("Введите координату y точки");
+            Double.TryParse(Console.ReadLine(),new CultureInfo("en-US"), out double y);
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Квадрант 1");
+                answer1++;
+            }
+            else if (x < 0 && y > 0) Console.WriteLine("Квадрант 2");
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Квадрант 3");
+                answer3++;
+            }
+            else if (x > 0 && y < 0) Console.WriteLine("Квадрант 4");
+            else Console.WriteLine("Точка находится на оси");
+        }
+        Console.WriteLine(answer1);
+        Console.WriteLine(answer3);
         // end
-
         return (answer1, answer3);
     }
     public (int, double) Task_3_8()
@@ -391,9 +614,31 @@ public class Program
         int answer = 0, n = 0;
 
         // code here;
-
+        int fl = 0;
+        while (fl == 0)
+        {   
+            int test=0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (i + 1 == 4)
+                {
+                    Console.WriteLine("Введите 4 оценку студента, если студент последний, введите после оценки stop в той же строке");
+                    string s = Console.ReadLine();
+                    if (s.Contains("stop")) fl = 1;
+                    int.TryParse(s, out int mark);
+                    if (mark != 2 && mark != 3) test++;
+                }
+                else
+                {
+                    Console.WriteLine($"Введите {i + 1} оценку студента");
+                    int.TryParse(Console.ReadLine(), out int mark);
+                    if (mark != 2 && mark != 3) test++;
+                }
+            }
+            if (test == 4) answer++;
+        }
         // end
-
+        Console.WriteLine(answer);
         return answer;
     }
     public (int, double) Task_3_11()
@@ -402,7 +647,7 @@ public class Program
         double avg = 0.0;
 
         // code here;
-
+        
         // end
 
         return (answer, avg);
